6eee5337feb3fa90eae31430c634df150f3583fb
Charles Smith <charles@charlesesmith.net>
2015-03-09 19:43:58 -0700

    hopefully fixed timestamps on posts.

--- db/migrate/20150310023004_add_time_stamps_to_posts.rb
+++ db/migrate/20150310023004_add_time_stamps_to_posts.rb
@@ -1,6 +1,8 @@
 class AddTimeStampsToPosts < ActiveRecord::Migration
   def change_table 
-      add_column(:posts, :created_at, :datetime)
-      add_column(:posts, :updated_at, :datetime)
+      add_column :posts, :created_at, :datetime
+      add_column :posts, :updated_at, :datetime
+      add_index :posts, :created_at
+      add_index :posts, :updated_at
   end
 end
