93cf5e6bb7c2bac9e42ac37a4f4eaf0b1e560fc9
Charles Smith <charles@charlesesmith.net>
2015-03-09 20:22:13 -0700

    removed bad migration to add timestamps.

--- db/migrate/20150310023004_add_time_stamps_to_posts.rb
+++
@@ -1,8 +0,0 @@
-class AddTimeStampsToPosts < ActiveRecord::Migration
-  def change_table 
-      add_column :posts, :created_at, :datetime
-      add_column :posts, :updated_at, :datetime
-      add_index :posts, :created_at
-      add_index :posts, :updated_at
-  end
-end
