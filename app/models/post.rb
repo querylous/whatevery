7dbe7054a16574e648e7d5345f30b44726ef71d6
Charles Smith <charles@charlesesmith.net>
2015-03-24 22:58:59 -0700

    honestly, I forgot what changes I've made at this point. Maybe I have add.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
    belongs_to :user
    default_scope -> { order(created_at: :desc) }
+
+   validates :title, presence: { message: "The post needs a title." }
+   validates :notes, presence: { message: "This post is about something, right?" } 
 end
