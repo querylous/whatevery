da2a086e7108507d9b8a7428bafb40c5d3e94799
Charles Smith <charles@charlesesmith.net>
2015-04-02 01:14:28 -0700

    destroy action not working correctly for posts, but have updated views and added form shim.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,34 +3,47 @@ class PostsController < ApplicationController
   respond_to :html
 
   def index
-     @post = Post.all
-     respond_with(@post)
+    @post = Post.all
+    respond_with(@post)
   end
   
   def show
-     @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
-     if user_signed_in?
-       @post = Post.new
-     else
-       redirect_to "/users/sign_in"
-       flash[:notice] = "Please sign in first."
-     end
+    if user_signed_in?
+      @post = Post.new
+    else
+      redirect_to "/users/sign_in"
+      flash[:notice] = "Please sign in first."
+    end
   end
 
   def create 
-     @post = Post.new(post_params)
-     @post.user = current_user
-     @post.save
-     respond_with(@post)
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save
+    respond_with(@post)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy 
+
+    redirect_to 'posts#index'
   end
 
   def update
+  end
+
+  def edit
+    if user_signed_in?
+      @post = Post.find(params[:id])
+    else
+      redirect_to "/users/sign_in"
+      flash[:notice] = "Please sign in first."
+    end
   end
 
   private
