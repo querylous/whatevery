d69572faa6697f39898f45d22cfdd9cbaf4edad0
Charles Smith <charles@charlesesmith.net>
2015-04-08 00:31:36 -0700

    updated logic for current_ep post helper.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,9 @@
 module PostsHelper
   def current_ep
-    Post.all.order("id asc").limit(20).where("file_link IS NOT ?", "").first
+    if Post.all.order("id asc").limit(20).where("file_link IS NOT ?", "").first.nil?
+      return "#"
+    else
+      return Post.all.order("id asc").limit(20).where("file_link IS NOT ?", "").first
+    end
   end
 end
