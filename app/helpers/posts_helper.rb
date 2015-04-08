8d8c58901a4bf66ceaa4d21b734fc802c5cb3161
Charles Smith <charles@charlesesmith.net>
2015-04-08 00:36:37 -0700

    changed query in post helper to work with postgres

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,9 @@
 module PostsHelper
   def current_ep
-    if Post.all.order("id asc").limit(20).where("file_link IS NOT ?", "").first.nil?
+    if Post.all.order("id asc").limit(20).where("file_link IS NOT NULL").first.nil?
       return "#"
     else
-      return Post.all.order("id asc").limit(20).where("file_link IS NOT ?", "").first
+      return Post.all.order("id asc").limit(20).where("file_link IS NOT NULL").first
     end
   end
 end
