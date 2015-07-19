96d5d9bb14caf79d38395dbd2e9484ad08fc0a19
Charles Smith <charles@charlesesmith.net>
2015-07-19 16:14:55 -0700

    fixed logic so that if there was no file link on most recent post the first one with a link would be returned.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,9 @@
 module PostsHelper
   def current_ep
-    if Post.all.order("id asc").limit(20).where("file_link IS NOT NULL").first.nil?
+    if Post.all.order("id asc").limit(20).where("bit_length(file_link) > 0").first.nil?
       return "#"
     else
-      x = Post.all.order("id asc").limit(20).where("file_link IS NOT NULL").first
+      x = Post.all.order("id asc").limit(20).where("bit_length(file_link) > 0").first
       return x.file_link
     end
   end
