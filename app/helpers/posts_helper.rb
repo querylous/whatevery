844182210232ce46a447296983e372a2f0ec53f0
Charles Smith <charles@charlesesmith.net>
2015-08-06 00:06:36 -0700

    changed so that only episodes that are posted will show up in the player.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
     if Post.all.order("id asc").limit(20).where("bit_length(file_link) > 0").first.nil?
       return "#"
     else
-      x = Post.all.order("id asc").limit(20).where("bit_length(file_link) > 0").first
+      x = Post.all.order("id asc").limit(20).where("bit_length(file_link) > 0 AND posted IS TRUE").first
       return x.file_link
     end
   end
